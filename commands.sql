1ffdea128702666351a9181c516f1ff3b39b83d2
Akash chaudhary <akash0005007@gmail.com>
2023-02-18 14:54:06 +0530

    refresh

--- commands.sql
+++
@@ -1,5 +0,0 @@
-create database udemy;
-use udemy;
-show tables;
-desc user;
-select * from user;
--- src/main/java/com/example/Book/repository/BookRepo.java
+++
@@ -1,7 +0,0 @@
-package com.example.Book.repository;
-
-import com.example.Book.model.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookRepo extends JpaRepository<Book,Long> {
-}
