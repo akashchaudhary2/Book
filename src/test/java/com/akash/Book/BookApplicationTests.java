ae55871af793d144df8f543934ed3a2a0c12db05
Akash chaudhary <akash0005007@gmail.com>
2023-02-27 17:45:37 +0530

    Update BookApplicationTests.java

--- src/test/java/com/akash/Book/BookApplicationTests.java
+++ src/test/java/com/akash/Book/BookApplicationTests.java
@@ -24,15 +24,16 @@ class BookApplicationTests {
 
     @Test
     public void testBooksLowestToHighest() {
-        List<Book> books = Arrays.asList(new Book("akash","akash@gmail.com", "java basic programing", 50.0), new Book("akash","akash@gmail.com", "programing in java", 100.0), new Book("napoleon","napolean@gmail.com", "Think and grow rich", 150.0));
+        List<Book> books = Arrays.asList(new Book("akash", "akash@gmail.com", "java basic programing", 50.0), new Book("akash", "akash@gmail.com", "programing in java", 100.0), new Book("napoleon", "napolean@gmail.com", "Think and grow rich", 150.0));
         when(bookRepo.findAll()).thenReturn(books);
         assertEquals(service.priceLowestToHighest().get(0), books.get(0));
         assertEquals(service.priceLowestToHighest().get(1), books.get(1));
         assertEquals(service.priceLowestToHighest().get(2), books.get(2));
     }
+
     @Test
-    public void  testBookWithinPriceRange(){
-        List<Book> books = Arrays.asList(new Book("akash","akash@gmail.com", "java basic programing", 50.0), new Book("akash","akash@gmail.com", "programing in java", 100.0));
+    public void testBookWithinPriceRange() {
+        List<Book> books = Arrays.asList(new Book("akash", "akash@gmail.com", "java basic programing", 50.0), new Book("akash", "akash@gmail.com", "programing in java", 100.0));
         when(bookRepo.findAll()).thenReturn(books);
         Integer size = service.bookWithinPriceRange(100).size();
         assertFalse(size.equals(1));
@@ -40,7 +41,6 @@ class BookApplicationTests {
         assertFalse(size.equals(3));
         assertEquals(service.bookWithinPriceRange(100).get(0),
                 books.get(0));
-
     }
 
 }
